506a9f1efb4b42d1845f8a8c0d5816aa748b2e0a
Raime <0961120962fb@gmail.com>
2020-03-13 15:10:31 +0200

    fix

--- ShowSortMasNameStudent.cpp
+++ ShowSortMasNameStudent.cpp
@@ -9,14 +9,18 @@ void ShowSortMasNameStudent(Students* Massive, int _CountStudents,int average)
 	{
 		for (size_t e = 0; e < countsubj; e++)
 		{
-			temp+=Massive[i].MasMarksOfStudents[e];
+			temp += Massive[i].MasMarksOfStudents[e];
 
 		}
 		if ((temp / countsubj) > average)
-		{
+			goto Sort;
+
+	}
+	return;
+		Sort:
 			for (size_t j = 0; j <= _CountStudents; j++)
 			{
-				for (size_t q = _CountStudents - 1; q > i; q--)
+				for (size_t q = _CountStudents - 1; q > j; q--)
 				{
 					if (strcmp(Massive[q].FirstName, Massive[q - 1].FirstName) < 0)
 
@@ -26,9 +30,9 @@ void ShowSortMasNameStudent(Students* Massive, int _CountStudents,int average)
 
 				}
 			}
-		}
+		
 		
 			
-	}
+	
 	
 }
